ebb78abcc027e1e602850b8eb8bc4a5268d80f9f
akinakee972-ship-it <akinakee972@gmail.com>
2026-02-03 13:40:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let multiplier=1.00;
+let balance=100;
+let bet=100;
+let interval;
+let crash=false;
+let cashOut=false;
+
+function starGame() {}
+    multiplier=1.00;
+    crash=false;
+    cashOut=false;
+
+    document.getElementById("plane").innerText=fly;
+    document.getElementById("status").innerText="Flyling...";
+    document.getElementById("multiplier").innerText="1.00x";
+
+    let plane=document.getElementById("plane")
+    plane.style.left="0px";
+    plane.style.bottom="0px";{
+
+    }
+
+    
